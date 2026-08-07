b1cd7cb6d2d64e8d17d51ea60e011cf3393ae59f
Harel <97512222+R-L-S123@users.noreply.github.com>
2026-08-07 16:46:51 +0300

    Update collage_generator.py

--- collage_generator.py
+++ collage_generator.py
@@ -22,9 +22,9 @@ def parse_transform(transform):
 
     scale = float(values[0])
 
-    x = float(values[4])
+    x = -float(values[4])
 
-    y = float(values[5])
+    y = -float(values[5])
 
 
     return scale, x, y
